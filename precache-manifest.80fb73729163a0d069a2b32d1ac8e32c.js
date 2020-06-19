2fe302eb6a1a21ebd95492556033e87a50fb150e
Cernadas <cernadas1021@gmail.com>
2020-06-19 17:20:44 -0400

    Updates

---
+++ precache-manifest.80fb73729163a0d069a2b32d1ac8e32c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d960a4a56dc53b5165babff700bccb5a",
+    "url": "/react-tutorial/index.html"
+  },
+  {
+    "revision": "c7ca728ba628b7ced4c9",
+    "url": "/react-tutorial/static/css/main.9f92e933.chunk.css"
+  },
+  {
+    "revision": "f3a8912e9639c9ea3ccc",
+    "url": "/react-tutorial/static/js/2.1bbe048d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tutorial/static/js/2.1bbe048d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7ca728ba628b7ced4c9",
+    "url": "/react-tutorial/static/js/main.a083e71f.chunk.js"
+  },
+  {
+    "revision": "d2980c81d16204d1adff",
+    "url": "/react-tutorial/static/js/runtime-main.fba2e450.js"
+  }
+]);
